58ba970697ba3af3ad500b3d3ce206bf58de4cc8
Виктор Гатаулин <gataulin_vv@ascon.ru>
2022-04-16 22:30:41 +0300

    master

--- Apps/Startup.cs
+++ Apps/Startup.cs
@@ -6,11 +6,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Apps.MVCApp.Application.Hendlers.Requests;
 using Apps.MVCApp.DI;
 using Apps.MVCApp.Models;
-using System.Reflection;
 
 namespace Apps.MVCApp
 {
@@ -20,59 +17,57 @@ namespace Apps.MVCApp
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-
-
             services.AddMediator(config =>
             {
                 config.MediatorHandlersRegister();
 
             });
 
-            //services.AddDbContext<MVCAppContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SQLServerConnection")));
-
             services.AddDbContext<MVCAppContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SQLLocalConnection")));
+
+            //services.AddDbContext<MVCAppContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SQLServerConnection")));
 
             //services.AddDbContext<MVCAppContext>(options => options.UseNpgsql(Configuration.GetConnectionString("PostGresSqlConnection")));
 
-
             services.AddIdentity<AppUser, IdentityRole>(options =>
             {
-
                 options.User.RequireUniqueEmail = false;
+              
                 options.Password.RequireDigit = false;
+                
                 options.Password.RequiredUniqueChars = 0;
+                
                 options.Password.RequireLowercase = false;
+                
                 options.Password.RequireNonAlphanumeric = false;
+                
                 options.Password.RequireUppercase = false;
+                
                 options.Password.RequiredLength = 3;
 
             }).AddEntityFrameworkStores<MVCAppContext>();
 
             services.AddControllersWithViews();
         }
-
-
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
 
+            app.UseRouting();
+         
+            app.UseAuthentication();
+            
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapFallbackToController("Index", "Home");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");               
 
             });
         }
